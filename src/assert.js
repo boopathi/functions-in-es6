5fa8b53ea438c546f3a332f07ac31b1e338c7717
Boopathi Rajaa <me@boopathi.in>
2015-08-15 01:24:40 +0530

    Add editorconfig

--- src/assert.js
+++ src/assert.js
@@ -3,11 +3,11 @@
 
   function assert(condition, message) {
     if (!condition) {
-        message = message || "Assertion failed";
-        if (typeof Error !== "undefined") {
-            throw new Error(message);
-        }
-        throw message; // Fallback
+      message = message || "Assertion failed";
+      if (typeof Error !== "undefined") {
+          throw new Error(message);
+      }
+      throw message; // Fallback
     }
   }
 
