c56938d0dd2f1c0731709d0a1a8c50bf4cb966a5
Boopathi Rajaa <me@boopathi.in>
2015-08-15 20:05:46 +0530

    Move calling tests to single file

---
+++ src/test.js
@@ -0,0 +1,27 @@
+;(function(scope) {
+  'use strict';
+
+  if ('undefined' !== typeof require) {
+    scope.assert = require('./assert');
+    scope.getFunctionType = require('./get-function-type');
+    scope.equispace = require('./equispace');
+    scope.checks = require('./tests.spec');
+  } else if ('undefined' !== typeof load) {
+    load('./assert.js');
+    load('./get-function-type.js');
+    load('./equispace.js');
+    load('./tests.spec.js');
+  }
+
+  scope.checks.forEach((result, fn) => {
+    let str = 'function' === typeof fn ? Function.prototype.toString.call(fn) : fn.toString();
+  	let type = scope.getFunctionType(fn);
+  	scope.assert(result, type);
+    let message = ["✓ ", scope.equispace(type, 15), str];
+    if ('undefined' !== typeof console)
+      console.log(...message);
+    else if ('undefined' !== typeof print)
+      print(...message);
+  });
+
+})(this);
