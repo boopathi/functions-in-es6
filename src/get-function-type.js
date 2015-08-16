89364840128f186c37593371683891faa2ca3e06
Boopathi Rajaa <me@boopathi.in>
2015-08-17 00:25:19 +0530

    Fix indentation

--- src/get-function-type.js
+++ src/get-function-type.js
@@ -1,45 +1,45 @@
 ;(function(scope) {
   'use strict';
 
-	const f = 'function';
-	const c = 'class';
-	const a = 'arrow';
-	const g = 'generator';
-	const m = 'method';
+  const f = 'function';
+  const c = 'class';
+  const a = 'arrow';
+  const g = 'generator';
+  const m = 'method';
 
-	// in case someone changes a function's toString method
-	const toString = Function.prototype.toString;
-	// because GeneratorFunction isn't available globally
-	const GeneratorFunction = function* () {}.constructor;
+  // in case someone changes a function's toString method
+  const toString = Function.prototype.toString;
+  // because GeneratorFunction isn't available globally
+  const GeneratorFunction = function* () {}.constructor;
   function getFunctionType(fn) {
 
     // check only functions
-  	if ('function' !== typeof fn) return typeof fn;
+    if ('function' !== typeof fn) return typeof fn;
 
-  	// functions, classes, and generators have prototypes
-  	if ('object' === typeof fn.prototype) {
+    // functions, classes, and generators have prototypes
+    if ('object' === typeof fn.prototype) {
 
-  		// generator function test
-  		if (fn instanceof GeneratorFunction) return g;
+      // generator function test
+      if (fn instanceof GeneratorFunction) return g;
 
-  		// explicitly declared as class
-  		if (toString.call(fn).startsWith('class')) return c;
+      // explicitly declared as class
+      if (toString.call(fn).startsWith('class')) return c;
 
-  		// empty prototype chains are plain functions
-  		// others are classes
-  		return Object.keys(fn.prototype).length ? c : f;
-  	}
+      // empty prototype chains are plain functions
+      // others are classes
+      return Object.keys(fn.prototype).length ? c : f;
+    }
 
-  	// otherwise it is an arrow, or method
-  	// or purposefully someone had deleted the prototype
+    // otherwise it is an arrow, or method
+    // or purposefully someone had deleted the prototype
 
-  	// methods (class and extended object literals)
-  	// they always have a name and start with their name
-  	if (fn.name && toString.call(fn).startsWith(fn.name)) return m;
+    // methods (class and extended object literals)
+    // they always have a name and start with their name
+    if (fn.name && toString.call(fn).startsWith(fn.name)) return m;
 
-  	// function expressions with prototypes removed always
+    // function expressions with prototypes removed always
     // start with the function keyword. All else is fat arrow
-  	return toString.call(fn).startsWith('function') ? f : a;
+    return toString.call(fn).startsWith('function') ? f : a;
   }
 
   // export for CommonJS (node)
